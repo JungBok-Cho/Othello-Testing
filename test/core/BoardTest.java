23f0cbc2ae4e16cb1f08c3562f2be0c3ef3a2311
loydp <54460504+loydp@users.noreply.github.com>
2021-04-14 12:53:07 -0700

    Initial unit test for init in BoardTest

--- test/core/BoardTest.java
+++ test/core/BoardTest.java
@@ -1,12 +1,30 @@
 package core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+
+import java.awt.Point;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
     @Test
-    void isFull() {
+    void Board_valid_33isWhite_shouldCreateNewBoardWithInitialPlacement() {
+        Board board = new Board();
+        // should create 8 x 8 board
+        Point point = new Point(3, 3);
+        Assertions.assertEquals(board.getSquareState(point), SquareState.WHITE);
     }
+
+    @Test
+    void Board_invalid_55IsEmpty_shouldCreateNewBoardWithInitialPlacement() {
+        Board board = new Board();
+        // should create 8 x 8 board
+        Point point = new Point(5, 5);
+        Assertions.assertEquals(board.getSquareState(point), SquareState.EMPTY);
+    }
+
 }
