84d3cd616183b0a01c4a5b7b2b6727dd5a372630
JungBok Cho <chojb2015@gmail.com>
2021-04-14 13:17:34 -0700

    Add Board_CreateBoard_Should_Initiazlie_Board in BoardTest.java

--- test/core/BoardTest.java
+++ test/core/BoardTest.java
@@ -3,7 +3,6 @@ package core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 import java.awt.Point;
 
 
@@ -20,11 +19,37 @@ class BoardTest {
     }
 
     @Test
-    void Board_invalid_55IsEmpty_shouldCreateNewBoardWithInitialPlacement() {
+    void Board_valid_55IsEmpty_shouldCreateNewBoardWithInitialPlacement() {
         Board board = new Board();
         // should create 8 x 8 board
         Point point = new Point(5, 5);
         Assertions.assertEquals(board.getSquareState(point), SquareState.EMPTY);
+    }
+
+    @Test
+    void Board_CreateBoard_Should_Initiazlie_Board() {
+        Board board = new Board();
+        int BOARD_LENGTH = 8;
+        int BOARD_WIDTH = 8;
+        int middleNum1 = BOARD_LENGTH / 2;
+        int middleNum2 = BOARD_LENGTH / 2 - 1;
+        Point point = new Point();
+
+        for (point.x = 0; point.x < BOARD_LENGTH; point.x++) {
+            for (point.y = 0; point.y < BOARD_WIDTH; point.y++) {
+                if(point.getX() == middleNum1 && point.getY() == middleNum1) {
+                    Assertions.assertEquals(board.getSquareState(point), SquareState.WHITE);
+                } else if(point.getX() == middleNum2 && point.getY() == middleNum2) {
+                    Assertions.assertEquals(board.getSquareState(point), SquareState.WHITE);
+                } else if(point.getX() == middleNum1 && point.getY() == middleNum2) {
+                    Assertions.assertEquals(board.getSquareState(point), SquareState.BLACK);
+                } else if(point.getX() == middleNum2 && point.getY() == middleNum1) {
+                    Assertions.assertEquals(board.getSquareState(point), SquareState.BLACK);
+                } else {
+                    Assertions.assertEquals(board.getSquareState(point), SquareState.EMPTY);
+                }
+            }
+        }
     }
 
 }
