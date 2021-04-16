04766cb302964186e2eebba169a208a8d65124f1
JungBok Cho <chojb2015@gmail.com>
2021-04-16 14:33:57 -0700

    Add ControllerTest.java to the project

---
+++ test/logic/ControllerTest.java
@@ -0,0 +1,16 @@
+package logic;
+
+import core.Board;
+import core.SquareState;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+
+}
