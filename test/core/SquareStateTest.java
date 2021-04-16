ee85e7bc216e228f21c2bdbe651db539926bfc11
kalya <82010576+ExceptionThrower@users.noreply.github.com>
2021-04-16 13:32:00 -0700

    Add SquareState testcases

--- test/core/SquareStateTest.java
+++ test/core/SquareStateTest.java
@@ -1,12 +1,129 @@
 package core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class SquareStateTest {
+    // Character type state information
+    char blackChar = '●';
+    char whiteChar = '○';
+    char pssblChar = '.';
+    char emptyChar = ' ';
+
+    // Character type state information
+    String blackStr = "●";
+    String whiteStr = "○";
+    String pssblStr = ".";
+    String emptyStr = " ";
+
+    // Check for Opposites
+    @Test
+    void verifyBlackSquareStateOpposite() {
+        SquareState squareState = SquareState.BLACK;
+        // should create 8 x 8 board
+
+        SquareState oppositeState = squareState.opposite();
+
+        // squareState should not be black
+        Assertions.assertNotEquals(oppositeState, SquareState.BLACK);
+
+        // squareState should be white
+        Assertions.assertEquals(oppositeState, SquareState.WHITE);
+    }
 
     @Test
-    void opposite() {
+    void verifyWhiteSquareStateOpposite() {
+        SquareState squareState = SquareState.WHITE;
+        // should create 8 x 8 board
+
+        SquareState oppositeState = squareState.opposite();
+        // squareState should not be white
+        Assertions.assertNotEquals(oppositeState, SquareState.WHITE);
+
+        //squareState should be black
+        Assertions.assertEquals(oppositeState, SquareState.BLACK);
     }
+
+    @Test
+    void verifyEmptySquareStateOpposite() {
+        SquareState squareState = SquareState.EMPTY;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.opposite(), SquareState.EMPTY);
+    }
+
+    // Check for Symbols
+    @Test
+    void verifyBlackSquareStateSymbol() {
+        SquareState squareState = SquareState.BLACK;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.symbol(), this.blackChar);
+    }
+
+    @Test
+    void verifyWhiteSquareStateSymbol() {
+        SquareState squareState = SquareState.WHITE;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.symbol(), this.whiteChar);
+    }
+
+    @Test
+    void verifyEmptySquareStateSymbol() {
+        SquareState squareState = SquareState.EMPTY;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.symbol(), this.emptyChar);
+    }
+
+    @Test
+    void verifyNextPossSquareStateSymbol() {
+        SquareState squareState = SquareState.PSSBL;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.symbol(), this.pssblChar);
+    }
+
+    // Check for Symbols of Opposite state
+    @Test
+    void verifyWhiteSquareStateOppositeSymbol() {
+        SquareState squareState = SquareState.WHITE;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.opposite().symbol(), this.blackChar);
+    }
+
+    @Test
+    void verifyBlackSquareStateOppositeSymbol() {
+        SquareState squareState = SquareState.BLACK;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.opposite().symbol(), this.whiteChar);
+    }
+
+    // Check toString values of states
+    @Test
+    void verifyBlackSquareStateToString() {
+        SquareState squareState = SquareState.BLACK;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.toString(), String.valueOf(this.blackChar));
+    }
+
+    @Test
+    void verifyWhiteSquareStateToString() {
+        SquareState squareState = SquareState.WHITE;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.toString(), this.whiteStr);
+    }
+
+    @Test
+    void verifyEmptySquareStateToString() {
+        SquareState squareState = SquareState.EMPTY;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.toString(), this.emptyStr);
+    }
+
+    @Test
+    void verifyPossibleSquareStateToString() {
+        SquareState squareState = SquareState.PSSBL;
+        // should create 8 x 8 board
+        Assertions.assertEquals(squareState.toString(), this.pssblStr);
+    }
+
 }
+
