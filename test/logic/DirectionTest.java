f330fb75868157bb3d0000a7e875d6de7e873c53
loydp <54460504+loydp@users.noreply.github.com>
2021-04-18 14:08:52 -0700

    Made test file for Direction, created test for .next function

---
+++ test/logic/DirectionTest.java
@@ -0,0 +1,27 @@
+package logic;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectionTest {
+
+    @Test
+    void Direction_next_Valid_ShouldReturnNewPointOnMethodCall() {
+        int[][] coordinates = {{-1, 0}, {+1, 0}, {0, -1}, {0, +1}, {-1, -1}, {+1, +1}, {+1, -1}, {-1, +1}};
+        Direction[] directions = Direction.values();
+
+        Point tempPoint;
+        Point directionPoint;
+
+        for (int i = 0; i < coordinates.length; i++) {
+            tempPoint = new Point(coordinates[i][0], coordinates[i][1]);
+            directionPoint = directions[i].next(new Point(0, 0));
+
+            assertEquals(tempPoint, directionPoint);
+        }
+
+    }
+}
