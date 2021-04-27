2a82165e91ab4459f78c4dc8354d40e023dbd15e
kalya <82010576+ExceptionThrower@users.noreply.github.com>
2021-04-27 15:32:07 -0700

    Added more valid testcases of DifficultyLevel.

--- test/logic/DifficultyLevelTest.java
+++ test/logic/DifficultyLevelTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class DifficultyLevelTest {
 
     @Test
-    void DifficultyLevel_description_Valid_Should_Set_Easy() {
+    void DifficultyLevel_description_level_Valid_Should_Set_Easy() {
         DifficultyLevel difficultyLevelInstance = DifficultyLevel.EASY;
 
         Assertions.assertEquals(difficultyLevelInstance.description(), DifficultyLevel.EASY.description());
@@ -14,21 +14,21 @@ class DifficultyLevelTest {
     }
 
     @Test
-    void DifficultyLevel_description_Valid_Should_Set_Normal() {
+    void DifficultyLevel_description_level_Valid_Should_Set_Normal() {
         DifficultyLevel difficultyLevelInstance = DifficultyLevel.NORMAL;
         Assertions.assertEquals(difficultyLevelInstance.description(), DifficultyLevel.NORMAL.description());
         Assertions.assertEquals(difficultyLevelInstance.level(), DifficultyLevel.NORMAL.level());
     }
 
     @Test
-    void DifficultyLevel_description_Valid_Should_Set_Hard() {
+    void DifficultyLevel_description_level_Valid_Should_Set_Hard() {
         DifficultyLevel difficultyLevelInstance = DifficultyLevel.HARD;
         Assertions.assertEquals(difficultyLevelInstance.description(), DifficultyLevel.HARD.description());
         Assertions.assertEquals(difficultyLevelInstance.level(), DifficultyLevel.HARD.level());
     }
 
     @Test
-    void DifficultyLevel_description_Valid_Should_Set_Heroic() {
+    void DifficultyLevel_description_level_Valid_Should_Set_Heroic() {
         DifficultyLevel difficultyLevelInstance = DifficultyLevel.HEROIC;
         Assertions.assertEquals(difficultyLevelInstance.description(), DifficultyLevel.HEROIC.description());
         Assertions.assertEquals(difficultyLevelInstance.level(), DifficultyLevel.HEROIC.level());
