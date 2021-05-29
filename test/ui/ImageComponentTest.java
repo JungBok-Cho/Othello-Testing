e915f657cd010d5a85b3a31afb6169c602f680a8
kalya <82010576+ExceptionThrower@users.noreply.github.com>
2021-05-29 16:07:56 -0700

    Test cases added in ImageComponentTest.

--- test/ui/ImageComponentTest.java
+++ test/ui/ImageComponentTest.java
@@ -1,9 +1,10 @@
 package ui;
 
 import java.awt.*;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ImageComponentTest {
 
@@ -25,5 +26,43 @@ public class ImageComponentTest {
         int imageHeight = -1;
         Dimension unExpectedSize = new Dimension(imageWidth, imageHeight);
         Assertions.assertNotEquals(imageComponent.getPreferredSize(), unExpectedSize);
+    }
+
+    @Test
+    void ImageComponent_Paint_Mock_Valid() {
+        final Image image = Toolkit.getDefaultToolkit().getImage("src/ui/images/rows.png");
+        ImageComponent imageComponent = new ImageComponent(image);
+
+        // create mock class
+        Graphics mockGraphics = mock(Graphics.class);
+
+        // Verify successful run of paint()
+        try
+        {
+            when(mockGraphics.drawImage(image, 0, 0, imageComponent)).thenReturn(true);
+            imageComponent.paint(mockGraphics);
+        }
+        catch(Exception ex)
+        {
+            Assertions.fail("ImageComponent.paint should not throw");
+        }
+    }
+
+    @Test
+    void ImageComponent_Paint_Mock_invalid() {
+        final Image image = Toolkit.getDefaultToolkit().getImage("src/ui/images/rows.png");
+        ImageComponent imageComponent = new ImageComponent(image);
+
+        // Verify successful run of paint()
+        try
+        {
+            Graphics graphics = null;
+            imageComponent.paint(graphics);
+            Assertions.fail("ImageComponent.paint should throw with invalid graphics object");
+        }
+        catch(Exception ex)
+        {
+            Assertions.assertEquals(ex.getMessage(), "Cannot invoke \"java.awt.Graphics.drawImage(java.awt.Image, int, int, java.awt.image.ImageObserver)\" because \"g\" is null");
+        }
     }
 }
