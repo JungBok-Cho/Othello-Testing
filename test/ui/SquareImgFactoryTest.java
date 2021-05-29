3c99141aa49ab648e1a533cccb5967a680a363c8
kalya <82010576+ExceptionThrower@users.noreply.github.com>
2021-05-29 15:58:23 -0700

    Made changes in SquareImgFactoryTest.

--- test/ui/SquareImgFactoryTest.java
+++ test/ui/SquareImgFactoryTest.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 public class SquareImgFactoryTest extends SquareImgFactory {
 
     @Test
-    void SquareImgFactory_SquareType_Valid_Should_be_Black(){
+    void SquareImgFactory_SquareType_Valid_Should_be_Black() {
         int imagesize = 22;
         final Image blackDiscImg = Toolkit.getDefaultToolkit().getImage("src/ui/images/black.png").getScaledInstance(imagesize, imagesize, Image.SCALE_SMOOTH);
         ImageComponent blackImageComponent = new ImageComponent(blackDiscImg);
@@ -29,7 +29,7 @@ public class SquareImgFactoryTest extends SquareImgFactory {
     }
 
     @Test
-    void SquareImgFactory_SquareType_Valid_Should_be_White(){
+    void SquareImgFactory_SquareType_Valid_Should_be_White() {
         int imagesize = 22;
         final Image whiteDiscImg = Toolkit.getDefaultToolkit().getImage("src/ui/images/white.png").getScaledInstance(imagesize, imagesize, Image.SCALE_SMOOTH);
         ImageComponent whiteImageComponent = new ImageComponent(whiteDiscImg);
@@ -37,7 +37,7 @@ public class SquareImgFactoryTest extends SquareImgFactory {
     }
 
     @Test
-    void SquareImgFactory_SquareType_Valid_Should_be_PssblBlk(){
+    void SquareImgFactory_SquareType_Valid_Should_be_PssblBlk() {
         int imageSize = 22;
         final Image pssblBlkDiscImg = Toolkit.getDefaultToolkit().getImage("src/ui/images/blackpssbl.png").getScaledInstance(imageSize, imageSize, Image.SCALE_SMOOTH);
         ImageComponent pssbleBlkImageComponent = new ImageComponent(pssblBlkDiscImg);
@@ -45,7 +45,7 @@ public class SquareImgFactoryTest extends SquareImgFactory {
     }
 
     @Test
-    void SquareImgFactory_SquareType_Valid_Should_be_PssblWht(){
+    void SquareImgFactory_SquareType_Valid_Should_be_PssblWht() {
         int imageSize = 22;
         final Image pssblWhtDiscImg = Toolkit.getDefaultToolkit().getImage("src/ui/images/whitepssbl.png").getScaledInstance(imageSize, imageSize, Image.SCALE_SMOOTH);
         ImageComponent pssbleWhtDiscImageComponent = new ImageComponent(pssblWhtDiscImg);
@@ -53,22 +53,10 @@ public class SquareImgFactoryTest extends SquareImgFactory {
     }
 
     @Test
-    void SquareImgFactory_SquareType_Valid_Should_be_Empty(){
+    void SquareImgFactory_SquareType_Valid_Should_be_Empty() {
         int imageSize = 22;
         final Image emptyDiscImg = Toolkit.getDefaultToolkit().getImage("src/ui/images/empty.png").getScaledInstance(imageSize, imageSize, Image.SCALE_SMOOTH);
         ImageComponent emptyDiscImageComponent = new ImageComponent(emptyDiscImg);
         Assertions.assertEquals(SquareImgFactory.buildSquare(SquareImgFactory.SquareType.EMPTY).getPreferredSize(), emptyDiscImageComponent.getPreferredSize());
     }
-//
-//    @Test
-//    void SquareImgFactory_SquareType_Valid_Should_be_Null() {
-//        int imageSize = 22;
-//        final Image emptyDiscImg = Toolkit.getDefaultToolkit().getImage("src/ui/images/empty.png").getScaledInstance(imageSize, imageSize, Image.SCALE_SMOOTH);
-//
-//        // create mock class
-//        SquareImgFactory mockSquareType = mock(SquareImgFactory.class);
-//        ImageComponent emptyDiscImageComponent = new ImageComponent(emptyDiscImg);
-//        when(SquareType.BLACK).thenReturn(null);
-//        Assertions.assertEquals(SquareImgFactory.buildSquare(SquareType.valueOf("")), null);
-//    }
 }
