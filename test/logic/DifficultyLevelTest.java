006c4f4e002865f73f046fbde140ddc8a44ca264
JungBok Cho <chojb2015@gmail.com>
2021-06-03 01:39:28 -0700

    Update DifficultyLevelTest.java

--- test/logic/DifficultyLevelTest.java
+++ test/logic/DifficultyLevelTest.java
@@ -2,6 +2,10 @@ package logic;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.awt.*;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class DifficultyLevelTest {
 
